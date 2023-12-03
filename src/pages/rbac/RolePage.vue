d7b946adacebe387e9b1641a9b1288b513600904
jericho <jericho_ph@qq.com>
2023-12-04 07:10:17 +0800

    增加角色列表搜索功能

--- src/pages/rbac/RolePage.vue
+++ src/pages/rbac/RolePage.vue
@@ -1,16 +1,42 @@
 <template>
-  <div class="q-pa-md">
-    <div class="row"></div>
+  <div class="q-pa-md search-form">
+    <div class="row">
+      <div class="col">
+        <span style="font-size: 20px">搜索</span>
+      </div>
+      <div class="col text-right">
+        <q-btn-group right>
+          <q-btn color="primary" label="搜索" icon="search" @click="fnSearch" />
+          <q-btn color="primary" label="重置" flat @click="fnReset" />
+        </q-btn-group>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-3">
+        <q-form @submit="fnSearch" @reset="fnReset" class="q-gutter-md">
+          <div class="row">
+            <div class="col">
+              <q-input
+                filled
+                v-model="name_search"
+                label="名称"
+                lazy-rules
+                :rules="[]"
+              />
+            </div>
+          </div>
+        </q-form>
+      </div>
+    </div>
     <div class="row">
       <div class="col">
         <q-table
           flat
           bordered
-          title="Treats"
+          title="角色列表"
           :rows="rows"
           :columns="columns"
           row-key="name"
-          dark
           color="amber"
         />
       </div>
@@ -23,6 +49,7 @@ import { rbacRoleListAjax } from "../../apis/rbac";
 export default {
   data() {
     return {
+      name_search: "",
       columns: [
         {
           name: "name",
@@ -51,6 +78,14 @@ export default {
         console.log(res);
       });
     },
+    fnSearch() {
+      rbacRoleListAjax({
+        name: this.name_search,
+      }).then((res) => {
+        console.log(res);
+      });
+    },
+    fnReset() {},
   },
 };
 </script>
